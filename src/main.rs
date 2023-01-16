ae97f321de285c63bd78aa64bb0d96689479397b
凤翼烈风天翔 <zwb843536229@gmail.com>
2023-01-16 17:06:00 +0800

    feat: update vec

--- src/main.rs
+++ src/main.rs
@@ -12,17 +12,15 @@ fn main() {
 
   // 类似的 c++的vec也是在内存中线性布局，当容量不足时，也会重新申请，导致之前的引用失效。
   // 如下的样例c++ code演示了这一问题
-  /**
-    c++ vector reference
-  {
-    vector<int> v;
-    v.resize(10);
-    v[0] = 100;
-    auto& r = v[0];
-    cout << &v[0] << " " << &r << endl;
-    v.resize(100000);
-    cout << &v[0] << " " << &r << endl;
-    cout << v[0] << " " << r << endl;
-  }
-  **/
+  //   c++ vector reference
+  // {
+  //   vector<int> v;
+  //   v.resize(10);
+  //   v[0] = 100;
+  //   auto& r = v[0];
+  //   cout << &v[0] << " " << &r << endl;
+  //   v.resize(100000);
+  //   cout << &v[0] << " " << &r << endl;
+  //   cout << v[0] << " " << r << endl;
+  // }
 }
